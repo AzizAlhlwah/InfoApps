6480fc43cc53bc753c74b2f697a5ddd089af75e3
Aziz <rhyfmedia@gmail.com>
2014-01-25 04:30:42 +0300

    Add

--- riges.php
+++ riges.php
@@ -1,8 +1,8 @@
 <?php
-	//require("dbc.php");
+	require("dbc.php");
 	
-	//$username = $_POST['username'];
-	//$pass 	  = $_POST['pass'];
+	$username = $_POST['username'];
+	$pass 	  = $_POST['pass'];
 	
-	echo "Hello Worlod....";
+	echo $username;
 ?>
